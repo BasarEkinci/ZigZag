94ecca1020bd0cf4cb20f05b8d42155115dad3b1
BasarEkinci <basarekinci02@outlook.com>
2023-09-06 11:17:40 +0300

    Cube Manager Added

--- ZigZag/Assets/GameFolders/_Scripts/CubeManager.cs
+++ ZigZag/Assets/GameFolders/_Scripts/CubeManager.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody rb;
+
+    private void Update()
     {
-        
+        if(transform.position.y <= -7f)
+            Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionExit(Collision other)
     {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            rb.isKinematic = false;
+        }
     }
 }
