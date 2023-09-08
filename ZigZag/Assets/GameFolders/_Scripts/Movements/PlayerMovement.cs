8396c15608a21c81de57f754b1d4e8812890140b
BasarEkinci <basarekinci02@outlook.com>
2023-09-08 15:38:43 +0300

    Player Movement Edited

--- ZigZag/Assets/GameFolders/_Scripts/Movements/PlayerMovement.cs
+++ ZigZag/Assets/GameFolders/_Scripts/Movements/PlayerMovement.cs
@@ -1,49 +1,36 @@
-using System;
 using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour
 {
-    private float speed = 5f;
+    private float speed = 5.5f;
     private Rigidbody rb;
     Vector3 direction;
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    private void Start()
-    {
-        direction = Vector3.forward;
-        speed = 5;
-    }
-
+    private void Awake() => rb = GetComponent<Rigidbody>();
+    private void Start() => direction = Vector3.forward;
     private void Update()
     {
-        if (transform.position.y < 0.749f)
+        if (transform.position.y < 0.74f)
         {
             GameManager.Instance.IsGameOver = true;
             rb.velocity = new Vector3(0, rb.velocity.y, 0);
         }
-        Debug.Log("\nx: " + rb.velocity.x.ToString("0") + " y: " + rb.velocity.y.ToString("0"));
         PlayerMover();
     }
-
     private void ChangeDirection()
     {
         if(GameManager.Instance.IsGameOver) return;
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            GameManager.Instance.Score++;
             SoundManager.Instance.PlayOneShot(1);
             if (direction == Vector3.forward)
                 direction = Vector3.left;
             else
                 direction = Vector3.forward;
-            GameManager.Instance.Score++;
+            
         }
     }
-
     private void PlayerMover()
     {
         if (!GameManager.Instance.IsGameStarted) return;
